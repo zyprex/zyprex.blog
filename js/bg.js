570a48fcfea9d073942c3674f1e9a7cd891a2f9c
zyprex <aintzy@foxmail.com>
2020-12-23 10:25:33 +0800

    rebuilding site 2020-12-23 Wed 10:25:33

--- js/bg.js
+++ js/bg.js
@@ -2,8 +2,8 @@ let isLoaded = false;
 function pageReadyCallBack() {
   isLoaded = true; //console.log('ready '+Date.now());
   if(localStorage.getItem('viewMode')=='2') return;
-  if (!$('bg_img').disabled){
-    let bgsrc = $('bg_img').getAttribute("data-bg");
+  if (!bg_img.disabled){
+    let bgsrc = bg_img.getAttribute("data-bg");
     document.body.style.backgroundImage = "url("+bgsrc+")";
   }
 }
@@ -16,18 +16,18 @@ function viewChg(ele,v) {
   localStorage.setItem('viewMode',v);
   if (v<2) {
     if (!v) {
-      $('bg_img').href = "/css/bgtr.css";
+      bg_img.href = "/css/bgtr.css";
     } else {
-      $('bg_img').href = "/css/bgco.css";
+      bg_img.href = "/css/bgco.css";
     }
-    $('bg_img').disabled = false;
-    let bgsrc = $('bg_img').getAttribute("data-bg");
+    bg_img.disabled = false;
+    let bgsrc = bg_img.getAttribute("data-bg");
     document.body.style.backgroundImage = "url("+bgsrc+")";
   } else if(v==2) {
-    $('bg_img').disabled = true;
+    bg_img.disabled = true;
     document.body.style.backgroundImage = "";
   }
-  $('view_mod_btn').querySelectorAll("*").forEach(i=>i.classList.remove('th-btn-a'));
+  view_mod_btn.querySelectorAll("*").forEach(i=>i.classList.remove('th-btn-a'));
   ele.classList.add('th-btn-a');
 }
 let viBtn = view_mod_btn.querySelectorAll("*");
